3166648f4d461836d6f77067ce32dea204176307
vmar97 <9950711+vmar97@users.noreply.github.com>
2021-03-26 16:24:38 +0200

    JSON tweaks

--- ACVillager.py
+++ ACVillager.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 from lxml import html
 import requests
-import re
+import json
 
 class villagerLookup:
         """Grab villager information from Nookipedia"""
@@ -13,21 +13,21 @@ class villagerLookup:
 
         @commands.command(pass_context=True)
         async def villager(self, ctx, villager):
-                data = discord.Embed(title=villager, url=url, color=title_color, description=quote)
+                apilink = requests.get('https://api.nookipedia.com/villagers?name=' + villager + '&api_key=' + api-key)
                 api-key = 'INSERT_API_HERE'
-                page = requests.get('https://api.nookipedia.com/villagers?name=' + villager + '&api_key=' + api-key)
-                attributes = ["url", "title_color", "image_url", "species", "personality", "gender", "birthday_month", "birthday_day", "sign", "quote", "phrase"]
-                if page.status_code == 404:
+                data = discord.Embed(title=villager, url=nookapi["url"], color=nookapi["title_color"], description=nookapi["quote"])
+                nookapi = json.loads(apilink.text)
+                if nookapi == "[]":
                         data.add_field(name="Error", value="Villager does not exist!")
                 else:
                         data.set_thumbnail(url=image_url)
-                        embed.add_field(name="Species", value=species, inline=True)
-						embed.add_field(name="personality", value=personality, inline=True)
-						embed.add_field(name="gender", value=gender, inline=True)
-						embed.add_field(name="Birthday", value=birthday_month + ' ' + str(birthday_day), inline=True)
-						embed.add_field(name="Sign", value=sign, inline=True)
-						embed.add_field(name="Catchphrase", value=phrase, inline=True)
-						embed.add_field(name="More info", value='Learn more on Nookipedia', url=url, inline=True)
+                        data.add_field(name="Species", value=nookapi["species"], inline=True)
+						data.add_field(name="Personality", value=nookapi["personality"], inline=True)
+						data.add_field(name="Gender", value=nookapi["gender"], inline=True)
+						data.add_field(name="Birthday", value=nookapi["birthday_month"] + ' ' + str(nookapi["birthday_day"]), inline=True)
+						data.add_field(name="Sign", value=nookapi["sign"], inline=True)
+						data.add_field(name="Catchphrase", value=nookapi["phrase"], inline=True)
+						data.add_field(name="More info", value='Learn more on Nookipedia', url=nookapi["url"], inline=True)
 
 def setup(bot):
 	bot.add_cog(villagerLookup(bot))
