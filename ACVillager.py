a1051a0913917f1b3f456124600790867b078b95
vmar97 <9950711+vmar97@users.noreply.github.com>
2021-03-26 16:25:57 +0200

    minor fix

--- ACVillager.py
+++ ACVillager.py
@@ -22,12 +22,12 @@ class villagerLookup:
                 else:
                         data.set_thumbnail(url=image_url)
                         data.add_field(name="Species", value=nookapi["species"], inline=True)
-						data.add_field(name="Personality", value=nookapi["personality"], inline=True)
-						data.add_field(name="Gender", value=nookapi["gender"], inline=True)
-						data.add_field(name="Birthday", value=nookapi["birthday_month"] + ' ' + str(nookapi["birthday_day"]), inline=True)
-						data.add_field(name="Sign", value=nookapi["sign"], inline=True)
-						data.add_field(name="Catchphrase", value=nookapi["phrase"], inline=True)
-						data.add_field(name="More info", value='Learn more on Nookipedia', url=nookapi["url"], inline=True)
+                        data.add_field(name="Personality", value=nookapi["personality"], inline=True)
+                        data.add_field(name="Gender", value=nookapi["gender"], inline=True)
+                        data.add_field(name="Birthday", value=nookapi["birthday_month"] + ' ' + str(nookapi["birthday_day"]), inline=True)
+                        data.add_field(name="Sign", value=nookapi["sign"], inline=True)
+                        data.add_field(name="Catchphrase", value=nookapi["phrase"], inline=True)
+                        data.add_field(name="More info", value='Learn more on Nookipedia', url=nookapi["url"], inline=True)
 
 def setup(bot):
 	bot.add_cog(villagerLookup(bot))
