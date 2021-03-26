46d732e1bdddbbe4126d52b6744ad7a922c35bde
vmar97 <9950711+vmar97@users.noreply.github.com>
2021-03-26 16:45:09 +0200

    Update ACVillager.py

--- ACVillager.py
+++ ACVillager.py
@@ -1,22 +1,17 @@
+from redbot.core import commands
 import discord
-from discord.ext import commands
-from lxml import html
 import requests
 import json
 
 class villagerLookup:
         """Grab villager information from Nookipedia"""
-		
-
-        def __init__(self, bot):
-                self.bot = bot
 
         @commands.command(pass_context=True)
         async def villager(self, ctx, villager):
                 apilink = requests.get('https://api.nookipedia.com/villagers?name=' + villager + '&api_key=' + api-key)
                 api-key = 'INSERT_API_HERE'
                 nookapi = json.loads(apilink.text)
-                data = discord.Embed(title=villager, url=nookapi["url"], color=nookapi["title_color"], description=nookapi["quote"])
+                data = discord.Embed(title=villager, url=nookapi["url"], color='# ' +nookapi["title_color"], description=nookapi["quote"])
                 if nookapi == "[]":
                         data.add_field(name="Error", value="Villager does not exist!")
                 else:
@@ -28,6 +23,3 @@ class villagerLookup:
                         data.add_field(name="Sign", value=nookapi["sign"], inline=True)
                         data.add_field(name="Catchphrase", value=nookapi["phrase"], inline=True)
                         data.add_field(name="More info", value='Learn more on Nookipedia', url=nookapi["url"], inline=True)
-
-def setup(bot):
-	bot.add_cog(villagerLookup(bot))
